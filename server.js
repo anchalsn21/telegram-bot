eddc43e0c14188d91115874ab7c724042456021c
Anchal <anchal@parastechnologies.com>
2021-07-16 00:51:24 +0530

    fixes

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-
-bot.on("text", (ctx) => ctx.reply(`${text}`));
+let ct = [];
+bot.on("text", (ctx) => {
+  ct.push(ctx.message);
+  ctx.reply(`${ctx.message}`);
+});
 bot.launch();
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
-app.get("/", (req, res) => res.send("Working"));
+app.get("/", (req, res) => res.json(ct));
 app.listen(process.env.PORT || 2527, () => console.log("Running on port 2527"));
